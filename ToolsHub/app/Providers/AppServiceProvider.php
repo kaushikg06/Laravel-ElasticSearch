eeac7b7b2fafb4a92f789f0272d0acc2085c56fc
kaushik <k.garikipati@productlabs.net>
2017-10-08 23:03:30 -0600

    ElasticSearch App

---
+++ ToolsHub/app/Providers/AppServiceProvider.php
@@ -0,0 +1,34 @@
+<?php
+namespace App\Providers;
+use App\Articles\ArticlesRepository;
+use App\Articles\ElasticsearchArticlesRepository;
+use App\Articles\EloquentArticlesRepository;
+use Elasticsearch\Client;
+use Elasticsearch\ClientBuilder;
+use Illuminate\Support\ServiceProvider;
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bind(ArticlesRepository::class, ElasticsearchArticlesRepository::class);
+        $this->app->bind(Client::class, function () {
+            return ClientBuilder::create()
+                ->setHosts(config('services.search.hosts'))
+                ->build();
+        });
+    }
+}
